6c75936d6199b665e8de76453b65a39704a9f887
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2025-09-12 12:24:11 +0000

    checked

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
 const numeroSenha = document.querySelector('parametro-senha__texto');
 let tamanhoSenha = 12;
-numeroSenha.textContent = tamanhoSenha; 
+numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelector('parametro-senha__botao');
+const botoes = document.querySelectorAll('parametro-senha__botao');
 
-const [0].onclick = diminuiTamanho;
+botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho() {
-    if (tamanhoSenha > 1){
+function diminuiTamanho(){
+    if (tamanhoSeha > 1){
         //tamanhoSenha = tamanhoSenha - 1
-        tamanhoSenha--;
+        tamanhoSenha --;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
-function aumentaTamanho() {
-    if (tamanhoSenha > 20){
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
         //tamanhoSenha = tamanhoSenha + 1
-        tamanhoSenha++;
+        tamanhoSenha ++;
     }
     numeroSenha.textContent = tamanhoSenha;
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = "0123456789"
+const simbolos = '!@%*?';
+
+geraSenha();
+
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
